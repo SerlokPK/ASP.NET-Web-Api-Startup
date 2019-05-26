84e82a2658ebe81f1e08ed4d1312183fab222b45
DaliborOletic <doletic1@gmail.com>
2019-05-26 17:45:17 +0200

    Entity framework setup finished.

---
+++ Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Repository.DbModels;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity, CityTransportContext context);
+
+        void Delete(T entity, CityTransportContext context);
+
+        void Edit(T entity, CityTransportContext context);
+
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate, CityTransportContext context);
+
+        IQueryable<T> GetAll(CityTransportContext context);
+    }
+}
